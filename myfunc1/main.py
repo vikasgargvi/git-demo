5ad2dfd1cad3da0b523adf645804df8c79517060
Vikas Garg <vikasgargvi@gmail.com>
2022-04-22 07:01:42 +0000

    change in both fun

--- myfunc1/main.py
+++ myfunc1/main.py
@@ -1,3 +1,4 @@
-def hello_world:
+def hello_world():
   print('Cloud Function 1')
   print("Clone Testing 3")
+  print("chane in func 1")
--- myfunc2/main.py
+++ myfunc2/main.py
@@ -1,3 +1,4 @@
-def func2:
+def func2():
   print("This is test cloud Function 2")
   print("find change 1")
+  print("change in func 2")
